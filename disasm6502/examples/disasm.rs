7b638e0118f39aa35d76219945d2a241e4f46694
W. Brian Gourlie <bgourlie@gmail.com>
2019-06-18 22:15:21 -0500

    Only need one address stack for unexplored assembly

--- disasm6502/examples/disasm.rs
+++ disasm6502/examples/disasm.rs
@@ -8,8 +8,10 @@ pub fn main() -> Result<(), Error> {
     pretty_env_logger::init();
     let mut f = File::open("./test_roms/6502_functional_test.bin").unwrap();
     let mut d = Disassembler::new(&mut f, 0x0, PC_START)?;
+    let mut instr_count = 0;
     while let Some((addr, instr)) = d.read()? {
-        info!("{:04X}: {:?}", addr + PC_START, instr);
+        instr_count += 1;
+        info!("{:06} {:04X}: {:?}", instr_count, addr + PC_START, instr);
     }
     Ok(())
 }
