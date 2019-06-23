f0f6fcb2579e3ca6249e5e6139fb1af52118d682
W. Brian Gourlie <bgourlie@gmail.com>
2019-06-23 09:41:48 -0500

    Opaque offsets (#1)
    * WIP
    
    * Fix offset normalization

--- disasm6502/examples/disasm.rs
+++ disasm6502/examples/disasm.rs
@@ -7,7 +7,7 @@ pub fn main() -> Result<(), Error> {
     const PC_START: u16 = 0x400;
     pretty_env_logger::init();
     let mut f = File::open("./test_roms/6502_functional_test.bin").unwrap();
-    let mut d = Disassembler::new(&mut f, 0x0, PC_START)?;
+    let mut d = Disassembler::new(&mut f, PC_START);
     let mut instr_count = 0;
     while let Some((addr, instr)) = d.read()? {
         instr_count += 1;
