35c3b4c1e35a44e1717d16e191c6df8d9f209cbc
W. Brian Gourlie <bgourlie@gmail.com>
2019-06-24 16:19:07 -0500

    Implement Iterator/IntoIterator

--- disasm6502/examples/disasm.rs
+++ disasm6502/examples/disasm.rs
@@ -6,11 +6,9 @@ use std::fs::File;
 pub fn main() -> Result<(), Error> {
     pretty_env_logger::init();
     let mut f = File::open("./test_roms/6502_functional_test.bin").unwrap();
-    let mut d = Disassembler::new(&mut f, 0x400, 0x400)?;
-    let mut instr_count = 0;
-    while let Some((addr, instr)) = d.read()? {
-        instr_count += 1;
-        info!("{:06} {:04X}: {:?}", instr_count, addr, instr);
+    let disassembler = Disassembler::new(&mut f, 0x400, 0x400)?;
+    for (addr, instr) in disassembler {
+        info!("{:04X}: {:?}", addr, instr);
     }
     Ok(())
 }
