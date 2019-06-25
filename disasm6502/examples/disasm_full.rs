9dcde4eb2a5c314178667431f79369d77ab321bb
W. Brian Gourlie <bgourlie@gmail.com>
2019-06-24 22:11:35 -0500

    wip

--- disasm6502/examples/disasm_full.rs
+++ disasm6502/examples/disasm_full.rs
@@ -5,7 +5,7 @@ use std::fs::File;
 pub fn main() -> Result<(), Error> {
     pretty_env_logger::init();
     let mut f = File::open("./test_roms/6502_functional_test.bin").unwrap();
-    let disassembly = Disassembly::from_rom(&mut f, 0x400, 0x400)?;
+    let disassembly = Disassembly::from_rom(&mut f, 0x0, 0x400)?;
     for i in 0..0xffff {
         if let Some(instr) = disassembly.display_at(i) {
             println!("{:04X}: {}", i, instr);
