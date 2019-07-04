94feea4987a9b70eccaa5fed74985216df6d363e
W. Brian Gourlie <bgourlie@gmail.com>
2019-07-03 19:15:30 -0500

    Rom loading progress

--- ui/src/lib.rs
+++ ui/src/lib.rs
@@ -1,69 +1,88 @@
-#![feature(custom_inner_attributes)]
-#![rustfmt::skip::macros(div, input, label)]
-
 #[macro_use]
 extern crate seed;
 
-use {log::debug, seed::prelude::*};
+use {
+    log::debug,
+    seed::prelude::*,
+    wasm_bindgen::JsCast,
+    web_sys::{Event, FileReader, HtmlInputElement},
+};
 
-// Model
+struct RomSelectionModel {
+    message: Option<String>,
+}
 
-struct Model {
-    rom_path: Option<String>,
+impl Default for RomSelectionModel {
+    fn default() -> Self {
+        RomSelectionModel { message: None }
+    }
+}
+
+enum Model {
+    RomSelection(RomSelectionModel),
+    RomLoaded,
 }
 
 impl Model {
-    fn rom_path_display(&self) -> String {
-        if let Some(rom_path) = &self.rom_path {
-            rom_path.to_string()
-        } else {
-            "None Selected".to_owned()
-        }
+    fn transition_to_rom_selection(&mut self, message: Option<String>) {
+        *self = Model::RomSelection(RomSelectionModel { message })
+    }
+
+    fn transition_to_rom_loaded(&mut self) {
+        *self = Model::RomLoaded
     }
 }
 
 impl Default for Model {
     fn default() -> Self {
-        Self { rom_path: None }
+        Model::RomSelection(RomSelectionModel::default())
     }
 }
 
-// Update
-
 #[derive(Clone)]
 enum Msg {
-    RomSelected(String),
+    RomSelected(Event),
 }
 
 fn update(msg: Msg, model: &mut Model, _: &mut Orders<Msg>) {
     match msg {
-        Msg::RomSelected(path) => {
-            if path.trim().is_empty() {
-                debug!("No file selected.");
-                model.rom_path = None;
+        Msg::RomSelected(event) => {
+            let file_input: HtmlInputElement = event.target().unwrap().dyn_into().unwrap();
+            if let Some(_file) = file_input.files().unwrap().get(0) {
+                debug!("file selected!");
+                model.transition_to_rom_loaded();
             } else {
-                debug!("Selected {}", path);
-                model.rom_path = Some(path);
+                debug!("No file selected!");
+                model.transition_to_rom_selection(Some("No file was selected".to_owned()));
             }
         }
     }
 }
 
-// View
-
 fn view(model: &Model) -> El<Msg> {
-    div!
-    [ label!
-        [ "ROM Location"
-        , input!
-            [ attrs!
-                { At::Type => "file"
-                }
-            , input_ev(Ev::Change, Msg::RomSelected)
+    match model {
+        Model::RomSelection(model) => div![
+            error_message(&model),
+            label![
+                "ROM Location",
+                input![
+                    attrs! { At::Type => "file"
+                    },
+                    raw_ev(Ev::Change, Msg::RomSelected)
+                ]
             ]
-        ]
-        , span![ model.rom_path_display() ]
-    ]
+        ],
+
+        Model::RomLoaded => div!["Rom loaded!"],
+    }
+}
+
+fn error_message(model: &RomSelectionModel) -> El<Msg> {
+    if let Some(ref message) = model.message {
+        span![message]
+    } else {
+        empty![]
+    }
 }
 
 #[wasm_bindgen]
