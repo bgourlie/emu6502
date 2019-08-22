d5d9a45d79f8b9c0ff46e59347ea34f7b7b790a4
W. Brian Gourlie <bgourlie@gmail.com>
2019-08-22 11:03:35 -0500

    wip

--- asm6502/src/parser/mod.rs
+++ asm6502/src/parser/mod.rs
@@ -1,13 +1,43 @@
-use crate::{Span, Token};
+use crate::Token;
+use nom::{Err, IResult, Needed};
 
-struct Line<'a> {
-    label: Option<(Span<'a>, &'a str)>,
-    action: Option<Action<'a>>,
-    comment: Option<(Span<'a>, &'a str)>,
+enum Line<'a> {
+    MacroInvocation(&'a str),
+    Directive(Directive<'a>),
+    Opcode,
 }
 
-enum Action<'a> {
-    Opcode(Span<'a>, &'a [Token<'a>]),
-    Directive(Span<'a>, &'a [Token<'a>]),
-    Macro(Span<'a>, &'a [Token<'a>]),
+enum Directive<'a> {
+    MacroDecl(&'a str),
+}
+
+fn identifier<'a>(input: &'a [Token<'a>]) -> IResult<&'a [Token<'a>], &'a str> {
+    map_next_token(input, |token| {
+        if let Token::Identifier(_, name) = token {
+            Some(*name)
+        } else {
+            None
+        }
+    })
+}
+
+fn macro_decl<'a>(input: &'a [Token<'a>]) -> IResult<&'a [Token<'a>], Directive<'a>> {
+    let (input, macro_name) = identifier(input)?;
+    map_next_token(input, |t| {
+        if let Token::MacroStart(_) = t {
+            Some(Directive::MacroDecl(macro_name))
+        } else {
+            None
+        }
+    })
+}
+
+fn map_next_token<'a, T, F>(input: &'a [Token<'a>], f: F) -> IResult<&'a [Token<'a>], T>
+where
+    F: FnOnce(&'a Token<'a>) -> Option<T>,
+{
+    input
+        .get(0)
+        .and_then(|token| f(token).map(|t| (&input[1..], t)))
+        .ok_or(Err::Incomplete(Needed::Size(1)))
 }
