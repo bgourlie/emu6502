904587fda4f7cda95697cab452c1f283d889b10d
W. Brian Gourlie <bgourlie@gmail.com>
2019-12-23 12:34:22 -0600

    Fix test

--- asm6502/src/resolver/tests.rs
+++ asm6502/src/resolver/tests.rs
@@ -2,7 +2,6 @@ use super::*;
 use crate::{parse, Lexer, Token};
 
 #[test]
-#[ignore]
 fn test_resolve_expr() {
     let tokens: Vec<Token> = Lexer::new("val = 2 * (3 + 4)\n").map(|i| i.token).collect();
     let (_, lines) = parse(&tokens).unwrap();
@@ -12,6 +11,5 @@ fn test_resolve_expr() {
         resolver.resolve_line(line).unwrap();
     }
 
-    unimplemented!()
-    //assert_eq!(Line::Equals("val", 14), resolver.lines[0])
+    assert_eq!(14, resolver.variables["val"])
 }
